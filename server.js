61348dc59bb31e26d358e6325cb6d663fd39a472
cemilcelik <cemilcelik84@gmail.com>
2017-12-26 10:11:10 +0300

    Initial commit.

---
+++ server.js
@@ -0,0 +1,19 @@
+require('./config/config.js');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = { app };
